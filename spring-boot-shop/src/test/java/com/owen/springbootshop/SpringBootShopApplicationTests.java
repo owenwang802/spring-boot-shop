b85afe4faca6f154b2ce4271c901d08f93a01451
owenwang802 <yunl34646@gmail.com>
2025-05-18 21:58:23 +0800

    Spring Boot Project Init

---
+++ spring-boot-shop/src/test/java/com/owen/springbootshop/SpringBootShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owen.springbootshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
